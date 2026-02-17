bc567e42d88750a09b08299a8c9ec157def5285b
HamzaQamar05 <129007482+HamzaQamar05@users.noreply.github.com>
2026-02-17 09:49:18 -0500

    Update app.py

--- src/trending_consumer/app.py
+++ src/trending_consumer/app.py
@@ -1,1 +1,11 @@
+import json
+
+def handler(event, context):
+    # event["Records"] contains SQS messages
+    for record in event.get("Records", []):
+        msg = json.loads(record["body"])
+        # TODO: Update trending counters in DB/Redis (or DynamoDB if you choose)
+        print("Watch event:", msg)
+
+    return {"ok": True}
 
